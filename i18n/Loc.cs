72e9d0238aa9e1cba38078c17e3e487410b4eeac
Simon Nishi McCorkindale <simon@funx.jp>
2020-06-07 21:40:37 +0900

    i18n updates for all languages

--- i18n/Loc.cs
+++ i18n/Loc.cs
@@ -16,6 +16,12 @@ namespace VFatumbot.BotLogic
         {
             dicts.Add("en", File.ReadLines($"i18n/en.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
             dicts.Add("ja", File.ReadLines($"i18n/ja.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("zh", File.ReadLines($"i18n/zh_tw.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("cn", File.ReadLines($"i18n/zh_cn.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("ru", File.ReadLines($"i18n/ru.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("ko", File.ReadLines($"i18n/ko.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("it", File.ReadLines($"i18n/it.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("es", File.ReadLines($"i18n/es.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
         }
 
         public static string g(string key, params object[] formatters)
