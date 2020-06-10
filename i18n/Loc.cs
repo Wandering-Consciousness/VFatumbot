e1c6eeb9d7c96335bb31183c1016e2d38ccc227f
Simon Nishi McCorkindale <simon@funx.jp>
2020-06-10 09:00:34 +0900

    Fix locale detecting logic because 4 letters like zh-HK etc weren't be picked up properly

--- i18n/Loc.cs
+++ i18n/Loc.cs
@@ -16,7 +16,11 @@ namespace VFatumbot.BotLogic
         {
             dicts.Add("en", File.ReadLines($"i18n/en.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
             dicts.Add("ja", File.ReadLines($"i18n/ja.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
-            dicts.Add("zh", File.ReadLines($"i18n/zh_tw.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("zh-tw", File.ReadLines($"i18n/zh_tw.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("zh-hk", File.ReadLines($"i18n/zh_tw.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("zh-sg", File.ReadLines($"i18n/zh_cn.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("zh-cn", File.ReadLines($"i18n/zh_cn.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
+            dicts.Add("zh", File.ReadLines($"i18n/zh_cn.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
             dicts.Add("cn", File.ReadLines($"i18n/zh_cn.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
             dicts.Add("ru", File.ReadLines($"i18n/ru.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
             dicts.Add("ko", File.ReadLines($"i18n/ko.tsv").Select(line => line.Split('\t')).ToDictionary(line => line[0], line => line[1]));
@@ -28,8 +32,9 @@ namespace VFatumbot.BotLogic
         {
             key = key.ToLower();
 
-            var cl = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName;
-            if (!dicts.ContainsKey(cl))
+            var clN = Thread.CurrentThread.CurrentUICulture.Name.ToLower();
+            var cl2 = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName.ToLower();
+            if (!dicts.ContainsKey(cl2) && !dicts.ContainsKey(clN))
             {
                 if (dicts["en"].ContainsKey(key))
                 {
@@ -41,9 +46,13 @@ namespace VFatumbot.BotLogic
                 }
             }
 
-            if (dicts[cl].ContainsKey(key))
+            if (dicts.ContainsKey(clN) && dicts[clN].ContainsKey(key))
             {
-                return string.Format(dicts[cl][key], formatters);
+                return string.Format(dicts[clN][key], formatters);
+            }
+            else if (dicts.ContainsKey(cl2) && dicts[cl2].ContainsKey(key))
+            {
+                return string.Format(dicts[cl2][key], formatters);
             }
 
             return key;
