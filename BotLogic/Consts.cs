5c584ed7fc4d0848a86e2a9472a97be0eb03c3c8
Simon Nishi McCorkindale <simon@funx.jp>
2020-06-17 21:24:38 +0900

    Fix quarantine typo

--- BotLogic/Consts.cs
+++ BotLogic/Consts.cs
@@ -3,7 +3,7 @@
     public class Consts
     {
 #if RELEASE_PROD
-        public const string APP_VERSION = "4.7.4";
+        public const string APP_VERSION = "4.7.5";
 #else
         public const string APP_VERSION = "4.8.0";
 #endif
