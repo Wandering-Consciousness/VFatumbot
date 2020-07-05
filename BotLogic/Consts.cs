db039e9d17f0d736578c2a5bb6cb12b925e1c7f9
Simon Nishi McCorkindale <simon@funx.jp>
2020-07-06 05:24:15 +0900

    Add Russian Telegram lobby link plus shorten Temporal buttons

--- BotLogic/Consts.cs
+++ BotLogic/Consts.cs
@@ -2,7 +2,7 @@
 {
     public class Consts
     {
-        private const string _appVersion = "4.8.2";
+        private const string _appVersion = "4.8.3";
 #if RELEASE_PROD
         public const string APP_VERSION = _appVersion;
 #else
