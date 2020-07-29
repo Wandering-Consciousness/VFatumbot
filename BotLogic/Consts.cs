915722029f0152edb890e6705b84cfa0fda0ad91
Simon Nishi McCorkindale <simon@funx.jp>
2020-07-30 03:49:10 +0900

    Bump v4.9.17

--- BotLogic/Consts.cs
+++ BotLogic/Consts.cs
@@ -2,7 +2,7 @@
 {
     public class Consts
     {
-        private const string _appVersion = "4.9.16";
+        private const string _appVersion = "4.9.17";
 #if RELEASE_PROD
         public const string APP_VERSION = _appVersion;
 #else
